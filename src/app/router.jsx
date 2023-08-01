a259faf5c9cab38e8214c4e152014eb910ca5d4b
EnricoSantarelli <63019527+EnricoSantarelli@users.noreply.github.com>
2023-08-01 08:00:22 -0300

    blocking routes based on localStorage 🤩🤩

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Cards from "./presentation/pages/cards/cards";
 import Transactions from "./presentation/pages/transactions/transactions";
 import Home from "./presentation/pages/home/home";
 import Options from "./presentation/pages/options/options";
 
-const Router = () => ([
-    <BrowserRouter>
-      <Routes>
-        <Route path="/deposit" element={<Cards action={'Depositar'}/>} />
-        <Route path="/" element={<Home />} />
-        <Route path="/withdraw" element={<Cards action={'Sacar'}/>} />
-        <Route path="/transactions" element={<Transactions />} />	
-        <Route path="/options" element={<Options currentBalance={0} />} />	
-      </Routes>
-    </BrowserRouter>
-  ]);
+const apiEndPoint = localStorage.getItem('apiEndPoint');
+
+const Router = () => {
+  return(<BrowserRouter>
+    <Routes>
+      <Route exact path="/deposit" element={apiEndPoint ? <Cards action={'Depositar'}/> : <Navigate to="/"/>}  />
+      <Route path="/" element={<Home />} />
+      <Route path="/withdraw" element={apiEndPoint ? <Cards action={'Sacar'}/> : <Navigate to="/"/>} />
+      <Route path="/transactions" element={apiEndPoint ? <Transactions /> : <Navigate to="/"/>} />	
+      <Route path="/options" element={<Options currentBalance={0} />} />	
+    </Routes>
+  </BrowserRouter>);
+}
 
 export default Router
